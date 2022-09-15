6f0a0c7253f3f3de1b4d0c6524d93114762b8cc5
Jerika Kanz <jerikacapila@gmail.com>
2022-09-15 20:13:47 +0200

    Add element for answer

--- scripts/form.js
+++ scripts/form.js
@@ -17,16 +17,15 @@ const submitHandler = (event) => {
 
 const createCard = (dataObject) => {
   const card = document.createElement("div");
-  const cardBookmark = document.createElement("div");
   const cardQuestion = document.createElement("p");
   const cardButton = document.createElement("a");
+  const cardAnswer = document.createElement("p");
   const cardCategory = document.createElement("ul");
 
   card.classList.add("card");
 
-  cardBookmark.classList.add("card__bookmark");
-  cardBookmark.innerHTML = `
-  <svg class="card__icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25"
+  card.innerHTML = `
+  <svg class="card__bookmark" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25"
   viewBox="0 0 30 30">
   <path d="M23,27l-8-7l-8,7V5c0-1.105,0.895-2,2-2h12c1.105,0,2,0.895,2,2V27z">
   </path>
@@ -39,14 +38,17 @@ const createCard = (dataObject) => {
   cardButton.classList.add("card__btn");
   cardButton.textContent = "Show Answer";
 
+  cardAnswer.classList.add("card__answer");
+  cardAnswer.textContent = dataObject.answer;
+
   cardCategory.classList.add("card__category");
   cardCategory.innerHTML = `
   <li class="card__item">${dataObject.tags}</li>
   `;
 
-  card.append(cardBookmark);
   card.append(cardQuestion);
   card.append(cardButton);
+  card.append(cardAnswer);
   card.append(cardCategory);
   main.append(card);
 };
