3c8bbbf9da331c43df4cc169a8fb35f4a4240c09
Jerika Kanz <76426580+jecaps@users.noreply.github.com>
2022-09-17 11:25:43 +0200

    Toggle button text and answer text
    * Set default text display
    
    * Create Function that changes button text and visibility of answer text on click
    
    * Animate Bookmark
    
    * Increase Margin

--- scripts/card.js
+++ scripts/card.js
@@ -1,7 +1,21 @@
 const bookmarks = document.querySelectorAll("[data-js='bookmark']");
+const answerBtn = document.querySelector(".card__btn");
+const answerText = document.querySelector(".card__answer");
 
+// Changes bookmarks color when clicked
 bookmarks.forEach((bookmark) => {
   bookmark.addEventListener("click", () => {
     bookmark.classList.toggle("card__bookmark--marked");
   });
 });
+
+// Changes button text and visibility of answer text when clicked
+
+const answerBtnHandler = () => {
+  answerText.classList.toggle("card__answer--shown");
+  answerBtn.textContent === "Show Answer"
+    ? (answerBtn.textContent = "Hide Answer")
+    : (answerBtn.textContent = "Show Answer");
+};
+
+answerBtn.addEventListener("click", answerBtnHandler);
